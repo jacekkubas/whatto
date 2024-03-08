f0678981da804c76aa22e7b8157a847cd7433a6f
Jacek Kubas <j.kubas@MacBookAir-j.kubas>
2024-03-08 15:22:13 +0100

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
+  
+
   return (
-    <div className="app">
-      nasza apka
+    <div>
+      <Header title="header" />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+const Header = ({title}) => {
+  return (
+    <div className="header">{title}</div>
+  )
+}
+
+export default Header
