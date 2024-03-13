6475bbc13682afc84c1e705245cd93ee3ee12516
Jacek Kubas <j.kubas@MacBookAir-j.kubas>
2024-03-13 11:28:01 +0100

    usestate

--- src/components/Apka/index.js
+++ src/components/Apka/index.js
@@ -1,71 +1,26 @@
+import React, {useState} from 'react';
 import "./apka.css";
 import ikona from './camera-shutter.png';
-import miniatura1 from './1.jpg';
-import miniatura2 from './2.webp';
-import miniatura3 from './3.jpg';
 
 const Apka = () => {
+  const [movieArray, setMovieArray] = useState([]);
 
-  const wyniki = document.querySelector(".wyniki");
-  const kliknij = document.querySelector(".flex-center p");
-  const ikonka = document.querySelector(".ikona");
-
-  let pokaz = function() {
-    setTimeout(() => {
-      wyniki.classList.toggle("schowane");
-      kliknij.classList.toggle("schowane");
-      ikonka.classList.toggle("pauza");
-    }, "1");
-  }
-
-  let movieArray = [];
-
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWYwYWQ2NzMxN2RmYTVmY2UwNDE4ZmFhMWIxNzRkMSIsInN1YiI6IjY1ZjAyMmJlZTE5ZGU5MDE0YmI4MDczNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jBN6Blu-VnBAxc-U8IQtkqJghoe3oDDLJhjnX4wMjTU'
-  }
-};
-
-  // for (let i = 0; i < 50; i++ ) {
-  //   fetch(`https://api.themoviedb.org/3/movie/top_rated?page=${i}`, options)
-  // .then(response => response.json())
-  // .then(response => movieArray.push(...response.results))
-  // .catch(err => console.error(err));
-  // }
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWYwYWQ2NzMxN2RmYTVmY2UwNDE4ZmFhMWIxNzRkMSIsInN1YiI6IjY1ZjAyMmJlZTE5ZGU5MDE0YmI4MDczNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jBN6Blu-VnBAxc-U8IQtkqJghoe3oDDLJhjnX4wMjTU'
+    }
+  };
   
-  fetch('https://api.themoviedb.org/3/movie/top_rated?page=1', options)
+  const pokaz = async () => {
+    await fetch('https://api.themoviedb.org/3/movie/top_rated?page=1', options)
     .then(response => response.json())
-    .then(response => movieArray.push(...response.results))
+    .then(response => setMovieArray([ ...movieArray, ...response.results]))
     .catch(err => console.error(err));
 
-    fetch('https://api.themoviedb.org/3/movie/top_rated?page=2', options)
-    .then(response => response.json())
-    .then(response => movieArray.push(...response.results))
-    .catch(err => console.error(err));
-
-    fetch('https://api.themoviedb.org/3/movie/top_rated?page=3', options)
-    .then(response => response.json())
-    .then(response => movieArray.push(...response.results))
-    .catch(err => console.error(err));
-
-    fetch('https://api.themoviedb.org/3/movie/top_rated?page=4', options)
-    .then(response => response.json())
-    .then(response => movieArray.push(...response.results))
-    .catch(err => console.error(err));
-
-    fetch('https://api.themoviedb.org/3/movie/top_rated?page=5', options)
-    .then(response => response.json())
-    .then(response => movieArray.push(...response.results))
-    .catch(err => console.error(err));
-
-    // let losowyFilm1;
-    // setTimeout(() => {
-    //   losowyFilm1 = movieArray[Math.floor(Math.random(movieArray.length) * 100)];
-    //   console.log(losowyFilm1);
-    // }, "100");
-    console.log(movieArray)
+    console.log(movieArray);
+  }
 
   return (
     <div className="Apka">
@@ -75,30 +30,21 @@ const options = {
       <div className="flex-center">
         <p>Kliknij aby wylosować film na dziś</p>
       </div>
-      <div className="wyniki schowane">
-        <h2>Filmy na dziś</h2>
-        <div className="wynik">
-          <img className="miniatura" src={miniatura1} />
-          <div className="tekst">
-            <h3>{movieArray[Math.floor(Math.random(movieArray.length) * 100)]}</h3>
-            <p>reż. Peter Jackson</p>
-          </div>
+      {movieArray.length > 0 && (
+        <div className="wyniki">
+          <h2>Filmy na dziś</h2>
+          {movieArray.map((movie) => (
+              <div className="wynik" key={movie.id}>
+                <img className="miniatura" src={movie.img} alt="" />
+                <div className="tekst">
+                  <h3>{movie.title}</h3>
+                  <p>{movie.overview}</p>
+                </div>  
+              </div>
+            )
+          )}
         </div>
-        <div className="wynik">
-          <img className="miniatura" src={miniatura2} />
-          <div className="tekst">
-            <h3>Harry Potter (2001)</h3>
-            <p>reż. Kokot</p>
-          </div>
-        </div>
-        <div className="wynik">
-          <img className="miniatura" src={miniatura3} />
-          <div className="tekst">
-            <h3>Oppenheimer (2023)</h3>
-            <p>reż. Christopher Nolan</p>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
