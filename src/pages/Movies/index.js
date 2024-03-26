3187ad6718ef75c5eb8bf57572ef254d6205e73f
Jacek Kubas <j.kubas@MacBookAir-j.kubas>
2024-03-26 09:05:19 +0100

    movies

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Apka from '../../components/Apka'
 
 const Movies = () => {
   return (
     <div className="page">
       <div className="container">
         <div>Movies</div>
+        <Apka />
       </div>
     </div>
   )
