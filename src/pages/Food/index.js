1539b8bf2c3949a99dac818520a4db79d7eff687
arturmx1 <mealgn3@o2.pl>
2024-03-27 08:23:17 +0100

    foodApi-1

--- src/pages/Food/index.js
+++ src/pages/Food/index.js
@@ -5,28 +5,20 @@ import {random} from '../../utils';
 
 const Food = () => {
   const [foodArray, setFoodArray] = useState([]);
-  const [page, setPage] = useState(random(1, 100));
 
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWYwYWQ2NzMxN2RmYTVmY2UwNDE4ZmFhMWIxNzRkMSIsInN1YiI6IjY1ZjAyMmJlZTE5ZGU5MDE0YmI4MDczNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jBN6Blu-VnBAxc-U8IQtkqJghoe3oDDLJhjnX4wMjTU'
-    }
-  };
+  const foodApiKey = "1f24950fbccf47cb97b9b51802c4dda7";
 
   const getFood = async () => {
-    console.log(page);
-    await fetch(`https://api.themoviedb.org/3/movie/top_rated?page=${page}`, options)
+    await fetch(`https://api.spoonacular.com/recipes/random?number=100&apiKey=${foodApiKey}`)
     .then(response => response.json())
     .then(response => {
-      const newArr = [response.results[random(0, 19)], response.results[random(0, 19)], response.results[random(0, 19)]];
-      setFoodArray([...newArr]);
+      const foodArray = [response.results[random(0, 99)], response.results[random(0, 99)], response.results[random(0, 99)]];
+      setFoodArray([...foodArray]);
+      console.log(foodArray);
     })
-    .catch(err => {console.error(err)});
-
-    setPage(random(1, 100));
+    .catch(err => console.error(err));
   }
+  console.log(foodArray);
 
   useEffect(() => {
     getFood();
@@ -37,14 +29,14 @@ const Food = () => {
       <Menu text="Dziś przygotuję:" active="food" />
       {foodArray.length > 0 && (
         <div className="wyniki">
-          {foodArray.map((movie) => (
+          {foodArray.map((food) => (
               <div className="wrapper">
-                <div className="wynik" key={movie.id}>
-                  <img className="miniatura" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
+                <div className="wynik" key={food.id}>
+                  <img className="miniatura" src={`https://image.tmdb.org/t/p/original/${food.poster_path}`} alt="" />
                   <div className="tekst">
-                    <h3>{`${movie.title}  (${(movie.release_date).slice(0, 4)})`}</h3>
-                    <div className="overview"><span>{movie.overview}</span></div>
-                    <strong><a target="_blank" className="zobacz-wiecej" href={`https://www.themoviedb.org/movie/${movie.id}`} rel="noreferrer">Zobacz więcej</a></strong>
+                    <h3>{`${food.title}  (${(food.release_date).slice(0, 4)})`}</h3>
+                    <div className="overview"><span>{food.overview}</span></div>
+                    <strong><a target="_blank" className="zobacz-wiecej" href={`https://www.thefooddb.org/food/${food.id}`} rel="noreferrer">Zobacz więcej</a></strong>
                   </div>  
                 </div>
               </div>
