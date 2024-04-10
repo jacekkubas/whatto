7e631490f17338cea83ec745c016cb6b96c7aa74
Jacek Kubas <jacekkubas@Jaceks-Air.localdomain>
2024-04-10 15:16:34 +0200

    ads

--- src/pages/Slider/index.js
+++ src/pages/Slider/index.js
@@ -1,45 +1,69 @@
-import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
 import foto1 from "./1.png";
 import foto2 from "./2.png";
 import foto3 from "./3.png";
 import foto4 from "./4.png";
-import './style.scss';
+import "./style.scss";
 
 const Slider = () => {
+  const photoArr = [foto1, foto2, foto3, foto4];
+  const [currPhoto, setCurrPhoto] = useState(0);
 
-    const photoArr = [foto1, foto2, foto3, foto4];
-    const [currPhoto, setCurrPhoto] = useState(0);
-
-    const handlePrev = () => {
-        setCurrPhoto(currPhoto - 1);
-        if (currPhoto < 1) {
-            setCurrPhoto(photoArr.length - 1)
-        }
+  const handlePrev = () => {
+    if (currPhoto < 1) {
+      setCurrPhoto(photoArr.length - 1);
+      return;
     }
 
-    const handleNext = () => {
-        setCurrPhoto(currPhoto + 1);
-        if (currPhoto > photoArr.length - 2) {
-            setCurrPhoto(0)
-        }
+    setCurrPhoto(currPhoto - 1);
+  };
+
+  const handleNext = (curr) => {
+    if (curr >= photoArr.length - 1) {
+      setCurrPhoto(0);
+      return;
     }
 
-    // const autoSlide = setInterval(handleNext, 5000)
+    setCurrPhoto(curr + 1);
+  };
 
-    // window.addEventListener("load", autoSlide);
+  //   const autoSlide = () => {
+  //     let curr = 0;
 
-    const pauseSlide = () => {
-        console.log('mouseover')
-    }
+  //     const id = setInterval(() => {
+  //       curr = curr + 1;
 
-    return (
-        <div className="slider" onMouseOver={pauseSlide}>
-            <button onClick={handlePrev}>prev</button>
-            <img src ={photoArr[currPhoto]} alt="" />
-            <button onClick={handleNext}>next</button>
-        </div>
-    )
-}
+  //       if (curr >= photoArr.length - 1) {
+  //         curr = 0;
+  //       }
+
+  //       handleNext(curr);
+  //     }, 1000);
+  //     intervalRef.current = id;
+  //   };
+
+  //   useEffect(() => {
+  //     autoSlide();
+  //   }, []);
+
+  const pauseSlide = () => {
+    console.log("pause");
+  };
+
+  return (
+    <div className="slider" onMouseOver={pauseSlide}>
+      <button onClick={handlePrev}>prev</button>
+      <img src={photoArr[currPhoto]} alt="" />
+      <button
+        onClick={() => {
+          handleNext(currPhoto);
+        }}
+      >
+        next
+      </button>
+    </div>
+  );
+};
 
 export default Slider;
