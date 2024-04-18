c12aaaea6ecbb7c49209d22b18fa6e4b4dbffa51
arturmx1 <mealgn3@o2.pl>
2024-04-18 14:59:55 +0200

    style

--- src/pages/Hotel/index.js
+++ src/pages/Hotel/index.js
@@ -5,48 +5,84 @@ import { mockData } from "./mock";
 const Hotel = () => {
   const [data, setData] = useState([]);
 
+  const [showReviews, setShowReviews] = useState(false);
+
   useEffect(() => {
     setTimeout(function () {
       setData(mockData);
     }, 50);
   }, []);
-  console.log(data);
+
+  const handleReviews = () => {
+    setShowReviews(!showReviews)
+  };
 
   return (
-    <div>
+    <div className="hotel">
+      <button className="button button-top">Load Hotels</button>
       {data &&
         data.map((hotel) => {
           return (
-            <div className="container hotel">
-              <button>Load Hotels</button>
-              <div className="hotel__box">
-                <div className="hotel__left">
-                  <img src={hotel.Photo} />
-                </div>
-                <div className="hotel__right">
-                  <div className="hotel__2columns">
-                    <div>
-                      <h2>{hotel.Name}</h2>
-                      <p>
-                        {hotel.City} - {hotel.Country}
-                      </p>
+            <div className="container">
+              <div className="single-hotel">
+                <div className="hotel__box">
+                  <div className="hotel__left">
+                    <img
+                      src={Object.values(hotel.Photo)[0]}
+                      alt={hotel.Name + " photo"}
+                    />
+                  </div>
+                  <div className="hotel__right">
+                    <div className="hotel__2columns">
+                      <div>
+                        <h2>{hotel.Name}</h2>
+                        <p>
+                          {hotel.City} - {hotel.Country}
+                        </p>
+                      </div>
+                      <div>
+                        <span className="stars">
+                          {"★ ".repeat(hotel.Stars) +
+                            "☆ ".repeat(5 - hotel.Stars)}
+                        </span>
+                      </div>
                     </div>
-                    <div>
-                      <span className="stars">
-                        {"★ ".repeat(hotel.Stars) +
-                          "☆ ".repeat(5 - hotel.Stars)}
-                      </span>
+                    <p className="description">{hotel.Description}</p>
+                    <div className="hotel__2columns bottom">
+                      <button className="button" onClick={handleReviews}>
+                        Show reviews
+                      </button>
+                      <div>
+                        <p className="hotel__align-right">
+                          Price: {hotel.Price} EUR per night
+                        </p>
+                        <p className="hotel__align-right">
+                          Available from{" "}
+                          {hotel.Available[0] + " to " + hotel.Available[1]}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                  <p className="description">{hotel.Description}</p>
-                  <div className="hotel__2columns">
-                    <button>Show reviews</button>
-                    <div>
-                      <p>{hotel.Price}</p>
-                      <p>{hotel.Available}</p>
-                    </div>
-                  </div>
                 </div>
+                {showReviews && (
+                  <div className="hotel__reviews opened">
+                    <div className="hotel__review">
+                      <div>
+                        <div className="hotel__rating">
+                          <p>+</p>
+                        </div>
+                      </div>
+                      <div>
+                        <p>
+                          <strong>{hotel.Review1Author}</strong>
+                        </p>
+                        <br />
+                        <p>{hotel.Review1}</p>
+                      </div>
+                    </div>
+                    <div className="hotel__separator"></div>
+                  </div>
+                )}
               </div>
             </div>
           );
--- src/pages/Hotel/mock.js
+++ src/pages/Hotel/mock.js
@@ -12,10 +12,15 @@ export const mockData = [
     Photo: { hotelPhoto1 },
     Description:
       " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultricies dui vel congue gravida. Morbi et erat eu urna gravida volutpat et posuere turpis.",
-    "Review Type": "positive",
-    Review:
+      Review1Author: "Mary Jane",
+      Review1isPositive: true,
+    Review1:
       "Fusce at enim vel ante efficitur porttitor. Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl. Sed diam nulla, fringilla mattis eleifend consequat, ornare eget velit.",
-    Stars: 3,
+      Review2Author: "John Doe",
+      Review2isPositive: false,
+    Review2:
+      "Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl.",
+      Stars: 3,
     Price: 123,
     Available: ["22-01-2020", "22-01-2021"],
   },
@@ -26,9 +31,14 @@ export const mockData = [
     Photo: { hotelPhoto2 },
     Description:
       "Proin ultricies dui vel congue gravida. Morbi et erat eu urna gravida volutpat et posuere turpis.",
-    "Review Type": "negative",
-    Review:
-      "Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl. Sed diam nulla, fringilla mattis eleifend consequat, ornare eget velit. Quisque vel cursus mi. Nullam sit amet augue in turpis fermentum sagittis. Praesent placerat fringilla vulputate. Praesent nulla libero, lobortis eget urna sed, sodales ultricies purus. Nulla sit amet dui lobortis, suscipit quam ac, aliquet mi. Proin scelerisque magna vel elit volutpat condimentum. Aenean quis rutrum est. Nam erat tellus, sollicitudin sit amet sagittis quis, auctor quis dui. Ut vel est vitae sapien mollis congue nec venenatis velit. Mauris a rutrum nisi, luctus vulputate nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla ultricies mi sit amet urna tempus viverra. Duis elementum justo diam, nec rutrum metus fringilla a. Cras quis odio arcu. In tempor tempor mi, sit amet gravida lectus interdum ac. In suscipit orci quis accumsan efficitur. Nulla urna leo, sagittis in nunc sed, fermentum pharetra dolor. Phasellus ultrices, risus et luctus varius, odio lacus tempor nisi, ut dictum tellus enim vitae dolor.",
+      Review1Author: "Mary Jane",
+      Review1isPositive: true,
+    Review1:
+      "Fusce at enim vel ante efficitur porttitor. Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl. Sed diam nulla, fringilla mattis eleifend consequat, ornare eget velit.",
+      Review2Author: "John Doe",
+      Review2isPositive: false,
+    Review2:
+      "Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl.",
     Stars: 2,
     Price: 103,
     Available: ["23-01-2020", "23-01-2021"],
@@ -40,9 +50,14 @@ export const mockData = [
     Photo: { hotelPhoto3 },
     Description:
       "Proin ultricies dui vel congue gravida. Morbi et erat eu urna gravida volutpat et posuere turpis.",
-    "Review Type": "positive",
-    Review:
-      "Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl. Sed diam nulla, fringilla mattis eleifend consequat, ornare eget velit. Quisque vel cursus mi. Nullam sit amet augue in turpis fermentum sagittis. Praesent placerat fringilla vulputate. Praesent nulla libero, lobortis eget urna sed, sodales ultricies purus. Nulla sit amet dui lobortis, suscipit quam ac, aliquet mi. Proin scelerisque magna vel elit volutpat condimentum. Aenean quis rutrum est. Nam erat tellus, sollicitudin sit amet sagittis quis, auctor quis dui. Ut vel est vitae sapien mollis congue nec venenatis velit. Mauris a rutrum nisi, luctus vulputate nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla ultricies mi sit amet urna tempus viverra. Duis elementum justo diam, nec rutrum metus fringilla a. Cras quis odio arcu. In tempor tempor mi, sit amet gravida lectus interdum ac. In suscipit orci quis accumsan efficitur. Nulla urna leo, sagittis in nunc sed, fermentum pharetra dolor. Phasellus ultrices, risus et luctus varius, odio lacus tempor nisi, ut dictum tellus enim vitae dolor.",
+      Review1Author: "Mary Jane",
+      Review1isPositive: true,
+    Review1:
+      "Fusce at enim vel ante efficitur porttitor. Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl. Sed diam nulla, fringilla mattis eleifend consequat, ornare eget velit.",
+      Review2Author: "John Doe",
+      Review2isPositive: false,
+    Review2:
+      "Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl.",
     Stars: 5,
     Price: 153,
     Available: ["24-01-2020", "24-01-2021"],
@@ -55,22 +70,33 @@ export const mockData = [
     Description:
       "Morbi et erat eu urna gravida volutpat et posuere turpis. Morbi et erat eu urna gravida volutpat et posuere turpis. Morbi et erat eu urna gravida volutpat et posuere turpis. Morbi et erat eu urna gravida volutpat et posuere turpis.",
     "Review Type": "positive",
-    Review:
-      "Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl. Sed diam nulla, fringilla mattis eleifend consequat, ornare eget velit. Quisque vel cursus mi. Nullam sit amet augue in turpis fermentum sagittis. Praesent placerat fringilla vulputate. Praesent nulla libero, lobortis eget urna sed, sodales ultricies purus. Nulla sit amet dui lobortis, suscipit quam ac, aliquet mi. Proin scelerisque magna vel elit volutpat condimentum. Aenean quis rutrum est. Nam erat tellus, sollicitudin sit amet sagittis quis, auctor quis dui. Ut vel est vitae sapien mollis congue nec venenatis velit. Mauris a rutrum nisi, luctus vulputate nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla ultricies mi sit amet urna tempus viverra. Duis elementum justo diam, nec rutrum metus fringilla a. Cras quis odio arcu. In tempor tempor mi, sit amet gravida lectus interdum ac. In suscipit orci quis accumsan efficitur. Nulla urna leo, sagittis in nunc sed, fermentum pharetra dolor. Phasellus ultrices, risus et luctus varius, odio lacus tempor nisi, ut dictum tellus enim vitae dolor.",
+    Review1Author: "Mary Jane",
+      Review1isPositive: true,
+    Review1:
+      "Fusce at enim vel ante efficitur porttitor. Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl. Sed diam nulla, fringilla mattis eleifend consequat, ornare eget velit.",
+      Review2Author: "John Doe",
+      Review2isPositive: false,
+    Review2:
+      "Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl.",
     Stars: 4,
     Price: 253,
     Available: ["25-01-2020", "25-01-2021"],
   },
   {
-    Name: "Hotel 4",
+    Name: "Hotel 5",
     Country: "Belgium",
     City: "Gent",
     Photo: { hotelPhoto5 },
     Description:
       "Proin ultricies dui vel congue gravida. Proin ultricies dui vel congue gravida. Proin ultricies dui vel congue gravida. Proin ultricies dui vel congue gravida.",
-    "Review Type": "positive",
-    Review:
-      "Praesent eget gravida nisl. Sed diam nulla, fringilla mattis eleifend consequat, ornare eget velit. Quisque vel cursus mi. Nullam sit amet augue in turpis fermentum sagittis. Praesent placerat fringilla vulputate. Praesent nulla libero, lobortis eget urna sed, sodales ultricies purus. Nulla sit amet dui lobortis, suscipit quam ac, aliquet mi. Proin scelerisque magna vel elit volutpat condimentum. Aenean quis rutrum est. Nam erat tellus, sollicitudin sit amet sagittis quis, auctor quis dui. Ut vel est vitae sapien mollis congue nec venenatis velit. Mauris a rutrum nisi, luctus vulputate nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla ultricies mi sit amet urna tempus viverra. Duis elementum justo diam, nec rutrum metus fringilla a. Cras quis odio arcu. In tempor tempor mi, sit amet gravida lectus interdum ac. In suscipit orci quis accumsan efficitur. Nulla urna leo, sagittis in nunc sed, fermentum pharetra dolor. Phasellus ultrices, risus et luctus varius, odio lacus tempor nisi, ut dictum tellus enim vitae dolor.",
+      Review1Author: "Mary Jane",
+      Review1isPositive: true,
+    Review1:
+      "Fusce at enim vel ante efficitur porttitor. Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl. Sed diam nulla, fringilla mattis eleifend consequat, ornare eget velit.",
+      Review2Author: "John Doe",
+      Review2isPositive: false,
+    Review2:
+      "Nullam ultricies libero sapien, ac interdum ipsum elementum a. Integer nec augue rhoncus, pulvinar odio laoreet, viverra dui. Praesent eget gravida nisl.",
     Stars: 5,
     Price: 143,
     Available: ["27-01-2020", "27-01-2021"],
